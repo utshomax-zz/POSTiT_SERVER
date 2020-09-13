262985221dd495a6155b41403bb60fd9f878117b
UTSAB UTSHO <utsab8008@gmail.com>
2020-09-13 08:52:49 +0530

    Update locdata.js

--- src/models/locdata.js
+++ src/models/locdata.js
@@ -5,66 +5,85 @@ const locSchema = new mongoose.Schema({
   b_id:String,
   name: String,
   oname: String,
+  mobile:Number,
+  d_loc:{
+    post_n:String,
+    post_no:String,
+    city:String,
+    dateTime:String
+  },
   c_loc:{
     post_n:String,
     post_no:String,
     city:String,
+    dateTime:String
   },
   
   0:{
       post_n:String,
       post_no:String,
       city:String,
+      dateTime:String
     },
   1:{
       post_n:String,
       post_no:String,
       city:String,
+      dateTime:String
     },
   2:{
       post_n:String,
       post_no:String,
       city:String,
+      dateTime:String
     },
   3:{
       post_n:String,
       post_no:String,
       city:String,
+      dateTime:String
     },
   4:{
       post_n:String,
       post_no:String,
       city:String,
+      dateTime:String
     },
   5:{
       post_n:String,
       post_no:String,
       city:String,
+      dateTime:String
     },
   6:{
       post_n:String,
       post_no:String,
       city:String,
+      dateTime:String
     },
   7:{
       post_n:String,
       post_no:String,
       city:String,
+      dateTime:String
     },
   8:{
       post_n:String,
       post_no:String,
       city:String,
+      dateTime:String
     },
   9:{
       post_n:String,
       post_no:String,
       city:String,
+      dateTime:String
     },
   10:{
     post_n:String,
     post_no:String,
     city:String,
+    dateTime:String
   }
  
 })
